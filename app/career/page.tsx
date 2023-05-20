4e15768296adace47035e2f6159ef46e3c5e7b13
Pardeep Singh <pardeep@logic-square.com>
2023-05-20 14:56:54 +0530

    achivement added

--- app/career/page.tsx
+++ app/career/page.tsx
@@ -25,10 +25,24 @@ const Career = () => {
     },
   ];
 
+  const achievements = [
+    {
+      title: "Going out of way to ensure team success",
+      companyName: "Logic Square Technologies",
+      description: `I received the award at Logic Square Technologies for “Going out of way to ensure team success"`,
+    },
+    {
+      title: "Hackathon",
+      companyName: "Logic Square Technologies",
+      description:
+        "I won Hackathon at Logic Square Technologies Company's Project.",
+    },
+  ];
+
   return (
     <div className="relative pb-16">
       <Navigation />
-      <div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
+      <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
         <div className="max-w-2xl mx-auto lg:mx-0">
           <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
             Career
@@ -53,6 +67,37 @@ const Career = () => {
                     <p className="text-gray-500">
                       {each.startDate} - {each.endDate}
                     </p>
+                  </div>
+                </div>
+              </article>
+            </Card>
+          ))}
+        </div>
+      </div>
+
+      <div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
+        <div className="max-w-2xl mx-auto lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
+            Achievement
+          </h2>
+        </div>
+
+        <div className="w-full h-px bg-zinc-800" />
+
+        <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
+          {achievements.map((each) => (
+            <Card key={each.title}>
+              <article className="p-4 md:p-8">
+                <h2 className="z-20 text-xl font-medium duration-1000 lg:text-xl text-zinc-200 group-hover:text-white font-display">
+                  {each.title}
+                </h2>
+                <p className="z-20 mt-4 text-md duration-1000 text-zinc-300 group-hover:text-zinc-200">
+                  {each.description}
+                </p>
+
+                <div className="flex items-center">
+                  <div className="text-sm">
+                    <p className="text-gray-500">{each.companyName}</p>
                   </div>
                 </div>
               </article>
