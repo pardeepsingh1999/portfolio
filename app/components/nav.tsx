354cca049554229151697781f219c2eb98447e2a
Pardeep Singh <pardeep@logic-square.com>
2023-10-18 09:41:16 +0530

    contact css fix

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -20,47 +20,47 @@ export const Navigation: React.FC = () => {
   return (
     <header ref={ref}>
       <div
-        className={`fixed inset-x-0 top-0 z-50 backdrop-blur duration-200 border-b  ${
+        className={`fixed inset-x-0 top-0 z-50 backdrop-blur duration-200 border-b ${
           isIntersecting
             ? "bg-zinc-900/0 border-transparent"
             : "bg-zinc-900/500  border-zinc-800 "
         }`}
       >
-        <div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
+        <div className="container flex flex-row-reverse items-center justify-between py-6 px-2 mx-auto">
           <div className="flex justify-between gap-2 text-xs md:gap-8 md:text-base">
             <Link
               href="/"
-              className="duration-200 text-zinc-400 hover:text-zinc-100"
+              className={`duration-200 text-zinc-400 hover:text-zinc-100`}
             >
               Home
             </Link>
             <Link
               href="/projects"
-              className="duration-200 text-zinc-400 hover:text-zinc-100"
+              className={`duration-200 text-zinc-400 hover:text-zinc-100`}
             >
               Projects
             </Link>
             <Link
               href="/expertise"
-              className="duration-200 text-zinc-400 hover:text-zinc-100"
+              className={`duration-200 text-zinc-400 hover:text-zinc-100`}
             >
               Expertise
             </Link>
             <Link
               href="/career"
-              className="duration-200 text-zinc-400 hover:text-zinc-100"
+              className={`duration-200 text-zinc-400 hover:text-zinc-100`}
             >
               Career
             </Link>
             <Link
               href="/education"
-              className="duration-200 text-zinc-400 hover:text-zinc-100"
+              className={`duration-200 text-zinc-400 hover:text-zinc-100`}
             >
               Education
             </Link>
             <Link
               href="/contact"
-              className="duration-200 text-zinc-400 hover:text-zinc-100"
+              className={`duration-200 text-zinc-400 hover:text-zinc-100`}
             >
               Contact
             </Link>
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -33,10 +33,10 @@ const socials = [
 
 export default function Example() {
   return (
-    <div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+    <div className="relative pb-16">
       <Navigation />
-      <div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
-        <div className="grid w-full grid-cols-1 gap-8 mx-auto sm:mt-0 sm:grid-cols-1 md:grid-cols-2">
+      <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
+        <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-2">
           {socials.map((s) => (
             <Card key={s.label}>
               <Link
